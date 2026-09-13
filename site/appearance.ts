8d06d8495aabbd95339196a575710075fbe52e1a
0thernet <ben@substrate.run>
2026-09-13 10:42:33 -0400

    Adopt shared material styling on Vhalla marketing site

---
+++ site/appearance.ts
@@ -0,0 +1,3 @@
+import { paletteColors } from "@hraness/design-kit";
+import { installAppearanceMenus } from "@hraness/design-kit/browser";
+installAppearanceMenus({ lightThemeColor: paletteColors.paper.light.background, darkThemeColor: paletteColors.paper.dark.background });
